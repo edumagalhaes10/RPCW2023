bbfc0d8f319b33ce626fecc60bcdac60bebbc0c6
Eduardo Magalhães <eduardomagalhaes107@gmail.com>
2023-03-01 00:06:55 +0000

    init TPC3

---
+++ TPC3/add_id.py
@@ -0,0 +1,20 @@
+import json
+ 
+def add_ID(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+
+    people = data['pessoas']
+    id = 1
+    for person in people:
+        person["id"] = 'p'+str(id)
+        id+=1
+
+    json_object = json.dumps(data, indent = 4,ensure_ascii=False) 
+
+
+    with open(filename, 'w') as f:
+        f.write(json_object)
+
+
+add_ID('datasets/dataset-extra1.json')
