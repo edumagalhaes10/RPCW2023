a8fa75c51fb8edeed97c2e6aa4d5b0ec3942bbf8
Eduardo Magalhães <eduardomagalhaes107@gmail.com>
2023-03-06 00:52:14 +0000

    update tpc3

--- TPC3/person_info.js
+++ TPC3/person_info.js
@@ -8,17 +8,41 @@ exports.person_info = function(person){
             <title>${person.nome}</title>
         </head>
         <body>
-            <div class="w3-card-4" style="width:50%;">
+            <div class="w3-card-4" style="width:100%;">
                 <header class="w3-container w3-blue-grey">
                     <h1>${person.nome}</h1>
                 </header>
 
                 <div class="w3-container">
-                    <p><b>Id: </b>${person.id}</p>
-                    <p><b>Idade: </b>${person.idade}</p>
-                    <p><b>Sexo: </b>${person.sexo}</p>
-                    <p><b>Cidade: </b>${person.morada.cidade}</p>
-                </div                
+    `
+
+    for (let item in person){
+        new_item = item.toLowerCase().replace(/_/g, ' ').replace(/\b\w/g, (letter) => letter.toUpperCase());
+       
+        if (typeof person[item] == 'object'){
+            htmlpage +=`
+                            <p><b>${new_item}: </b></p>
+                            <ul>
+            `
+            for(let item2 in person[item]){
+                new_item2 = item2.toLowerCase().replace(/_/g, ' ').replace(/\b\w/g, (letter) => letter.toUpperCase());
+                aux = person[item]
+                htmlpage += `
+                                    <p><b>${new_item2}: </b>${aux[item2]}</p>
+
+                `            
+            }
+            htmlpage += `               </ul>`
+        }
+        else{
+            htmlpage += `
+                            <p><b>${new_item}: </b>${person[item]}</p>
+            
+            `
+        }
+    } 
+    `
+                </div>               
             </div>
         </body>
     </html>
